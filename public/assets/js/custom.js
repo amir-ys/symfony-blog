1c9111f4491a083dbb6bc17389ecd43ee2815fa6
amir-ys <amir.you74@gmail.com>
2024-04-02 01:04:26 +0330

    feat(layouts) add layout for twig views

---
+++ public/assets/js/custom.js
@@ -0,0 +1,71 @@
+'use strict';
+
+(function ($) {
+
+    $(document).on('click', '.layout-builder .layout-builder-toggle', function () {
+        $('.layout-builder').toggleClass('show');
+    });
+
+    $(window).on('load', function () {
+        setTimeout(function () {
+            $('.layout-builder').removeClass('show');
+        }, 500);
+    });
+
+    var site_layout = localStorage.getItem('site_layout');
+    $('body').addClass(site_layout);
+
+    $('.layout-builder .layout-builder-body input[type="radio"][data-layout="' + $('body').attr('class') + '"]').prop('checked', true);
+
+    $('.layout-builder .layout-builder-body input[type="radio"]').click(function () {
+        var class_names = '';
+
+        $('.layout-builder .layout-builder-body input[type="radio"]:checked').each(function () {
+            class_names += ' ' + $(this).data('layout');
+        });
+
+        localStorage.setItem('site_layout', class_names);
+
+        window.location.href = (window.location.href).replace('#', '');
+    });
+
+    $(document).on('click', '#btn-layout-builder', function () {
+
+    });
+
+    $(document).on('click', '#btn-layout-builder-reset', function () {
+        localStorage.removeItem('site_layout');
+        localStorage.removeItem('site_layout_dark');
+
+        window.location.href = (window.location.href).replace('#', '');
+    });
+
+    $(window).on('load', function () {
+        if ($('body').hasClass('horizontal-side-menu') && $(window).width() > 768) {
+            if ($('body').hasClass('layout-container')) {
+                $('.side-menu .side-menu-body').wrap('<div class="container"></div>');
+            } else {
+                $('.side-menu .side-menu-body').wrap('<div class="container-fluid"></div>');
+            }
+            setTimeout(function () {
+                $('.side-menu .side-menu-body > ul').append('<li><a href="#"><span>سایر</span></a><ul></ul></li>');
+            }, 100);
+            $('.side-menu .side-menu-body > ul > li').each(function () {
+                var index = $(this).index(),
+                    $this = $(this);
+                if (index > 7) {
+                    setTimeout(function () {
+                        $('.side-menu .side-menu-body > ul > li:last-child > ul').append($this.clone());
+                        $this.addClass('d-none');
+                    }, 100);
+                }
+            });
+        }
+    });
+
+    $(document).on('click', '[data-attr="layout-builder-toggle"]', function () {
+        $('.layout-builder').toggleClass('show');
+        return false;
+    });
+
+})(jQuery);
